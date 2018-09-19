b6035c3fb617464f95decacb103736ea5aa47afc
Akiva Ranells <akranells@gmail.com>
2018-09-19 22:35:53 +0300

    links and routes working

--- src/components/Fentity.js
+++ src/components/Fentity.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import '../styles/fentity.css'
 
 
 class Fentity extends Component {
     render() {
+        console.log(this.props.match)
+        const fentityName = this.props.match.params.name;
+        const fentityCategory = this.props.match.params.fentities;
+        const fentity = this.props.state[fentityCategory].filter(f=>{
+            return f.name.toLowerCase()===fentityName.toLowerCase();
+        })[0];
+        const returnLink =`/directory/${fentityCategory}`
         return (
             <div id="creature-container">
+                <Link to={returnLink}>Bestiary</Link>
                 <h1>{fentity.name}</h1>
                 <img src={fentity.imgUrl} alt=""/>
                 <div className="title">Power:</div>
